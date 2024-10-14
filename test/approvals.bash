a81192ca0253556f13a62f2cc534812573fd734a
Filip Gołaś <filip.golas@3mdeb.com>
2024-10-14 12:43:58 +0200

    approvals.bash: Update to v0.5.1
    Signed-off-by: Filip Gołaś <filip.golas@3mdeb.com>

--- test/approvals.bash
+++ test/approvals.bash
@@ -1,7 +1,10 @@
-# approvals.bash v0.3.3
+# approvals.bash v0.5.1
 #
 # Interactive approval testing for Bash.
 # https://github.com/DannyBen/approvals.bash
+#
+# shellcheck disable=SC2059
+# Disabling SC2059 (quoted format string) because we use dynamic format strings
 approve() {
   local expected approval approval_file actual cmd
   approvals_dir=${APPROVALS_DIR:=approvals}
@@ -9,6 +12,10 @@ approve() {
   cmd=$1
   last_exit_code=0
   actual=$(eval "$cmd" 2>&1) || last_exit_code=$?
+  if [[ -v allow_diff_regex && "$allow_diff_regex" ]]; then
+    actual=$(echo "$actual" | sed -E "s/$allow_diff_regex/*/g")
+    unset allow_diff_regex
+  fi
   approval=$(printf "%b" "$cmd" | tr -s -c "[:alnum:]" _)
   approval_file="$approvals_dir/${2:-"$approval"}"
 
@@ -17,9 +24,9 @@ approve() {
   if [[ -f "$approval_file" ]]; then
     expected=$(cat "$approval_file")
   else
-    echo "--- [$(blue "new: $cmd")] ---"
+    printf -- "$new_diff_string\n" "$cmd"
     printf "%b\n" "$actual"
-    echo "--- [$(blue "new: $cmd")] ---"
+    printf -- "$new_diff_string\n" "$cmd"
     expected="$actual"
     user_approval "$cmd" "$actual" "$approval_file"
     return
@@ -28,30 +35,36 @@ approve() {
   if [[ "$(printf "%b" "$actual")" = "$(printf "%b" "$expected")" ]]; then
     pass "$cmd"
   else
-    echo "--- [$(blue "diff: $cmd")] ---"
+    printf -- "$changed_diff_string\n" "$cmd"
     $diff_cmd <(printf "%b" "$expected\n") <(printf "%b" "$actual\n") | tail -n +4
-    echo "--- [$(blue "diff: $cmd")] ---"
+    printf -- "$changed_diff_string\n" "$cmd"
     user_approval "$cmd" "$actual" "$approval_file"
   fi
 }
 
+allow_diff() {
+  allow_diff_regex="$1"
+}
+
 describe() {
-  echo
-  blue "= $*"
+  printf "$describe_string\n" "$*"
 }
 
 context() {
-  echo
-  magenta "= $*"
+  printf "$context_string\n" "$*"
+}
+
+it() {
+  printf "$it_string\n" "$*"
 }
 
 fail() {
-  red "  FAILED: $*"
+  printf "$fail_string\n" "$*"
   exit 1
 }
 
 pass() {
-  green "  approved: $*"
+  printf "$pass_string\n" "$*"
   return 0
 }
 
@@ -59,31 +72,61 @@ expect_exit_code() {
   if [[ $last_exit_code == "$1" ]]; then
     pass "exit $last_exit_code"
   else
-    fail "Expected exit code $1, got $last_exit_code"
+    fail "expected exit code $1, got $last_exit_code"
   fi
 }
 
-red() { printf "\e[31m%b\e[0m\n" "$*"; }
-green() { printf "\e[32m%b\e[0m\n" "$*"; }
-blue() { printf "\e[34m%b\e[0m\n" "$*"; }
-magenta() { printf "\e[35m%b\e[0m\n" "$*"; }
-cyan() { printf "\e[36m%b\e[0m\n" "$*"; }
-
 # Private
+
+print_in_color() {
+  local color="$1"
+  shift
+  if [[ -z ${NO_COLOR+x} ]]; then
+    printf "$color%b\e[0m\n" "$*"
+  else
+    printf "%b\n" "$*"
+  fi
+}
+
+red() { print_in_color "\e[31m" "$*"; }
+green() { print_in_color "\e[32m" "$*"; }
+yellow() { print_in_color "\e[33m" "$*"; }
+blue() { print_in_color "\e[34m" "$*"; }
+magenta() { print_in_color "\e[35m" "$*"; }
+cyan() { print_in_color "\e[36m" "$*"; }
+bold() { print_in_color "\e[1m" "$*"; }
+underlined() { print_in_color "\e[4m" "$*"; }
+red_bold() { print_in_color "\e[1;31m" "$*"; }
+green_bold() { print_in_color "\e[1;32m" "$*"; }
+yellow_bold() { print_in_color "\e[1;33m" "$*"; }
+blue_bold() { print_in_color "\e[1;34m" "$*"; }
+magenta_bold() { print_in_color "\e[1;35m" "$*"; }
+cyan_bold() { print_in_color "\e[1;36m" "$*"; }
+red_underlined() { print_in_color "\e[4;31m" "$*"; }
+green_underlined() { print_in_color "\e[4;32m" "$*"; }
+yellow_underlined() { print_in_color "\e[4;33m" "$*"; }
+blue_underlined() { print_in_color "\e[4;34m" "$*"; }
+magenta_underlined() { print_in_color "\e[4;35m" "$*"; }
+cyan_underlined() { print_in_color "\e[4;36m" "$*"; }
 
 user_approval() {
   local cmd="$1"
   local actual="$2"
   local approval_file="$3"
 
-  if [[ -v CI || -v GITHUB_ACTIONS ]]; then
+  if [[ -v CI || -v GITHUB_ACTIONS ]] && [[ -z "${AUTO_APPROVE+x}" ]]; then
     fail "$cmd"
   fi
 
-  echo
-  printf "[A]pprove? \n"
-  response=$(bash -c "read -n 1 key; echo \$key")
-  printf "\r"
+  if [[ -v AUTO_APPROVE ]]; then
+    response=a
+  else
+    echo
+    printf "$approval_string"
+    response=$(bash -c "read -n 1 key; echo \$key")
+    printf "\b%.s" $(seq 1 $((${#approval_string} + 1)))
+  fi
+
   if [[ $response =~ [Aa] ]]; then
     printf "%b\n" "$actual" >"$approval_file"
     pass "$cmd"
@@ -95,20 +138,32 @@ user_approval() {
 onexit() {
   exitcode=$?
   if [[ "$exitcode" == 0 ]]; then
-    green "\nFinished successfully"
+    printf "$exit_success_string\n" "$0"
   else
-    red "\nFinished with failures"
+    printf "$exit_failed_string\n" "$0"
   fi
+  echo
   exit $exitcode
 }
 
 onerror() {
-  fail "Caller: $(caller)"
+  fail "caller: $(caller)"
 }
 
 set -e
 trap 'onexit' EXIT
 trap 'onerror' ERR
+
+describe_string="$(bold ▌ describe)   %s"
+context_string="$(bold ▌ context)    %s"
+it_string="$(bold ▌ it)         %s"
+fail_string="  $(red_bold failed)     %s"
+pass_string="  $(green approved)   %s"
+exit_success_string="$(green ▌ exit)       $(bold %s finished successfully)"
+exit_failed_string="$(red_bold ▌ exit)       $(bold %s finished with errors)"
+new_diff_string="────┤ $(yellow new): $(bold %s) ├────"
+changed_diff_string="────┤ $(blue changed): $(bold %s) ├────"
+approval_string="[A]pprove? "
 
 if diff --help | grep -- --color >/dev/null 2>&1; then
   diff_cmd="diff --unified --color=always"
